1c91ec0a93698dfb331399e1de023b57f885698a
Alextony Rodrigues Teixeira <alextonyrodrigues@gmail.com>
2021-04-15 14:42:18 -0300

    Update Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,12 +12,9 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		/*
-		 * Department obj = new Department(1, "Books");
-		 * 
-		 * Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0,
-		 * obj);
-		 */
+		// Department obj = new Department(1, "Books");
+
+	//	Seller seller = new Seller(null, null, null, null, null, null);
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println("=== TEST 1: seller findById ===");
@@ -31,19 +28,25 @@ public class Program {
 			System.out.println(obj);
 		}
 
-			System.out.println("\n=== TEST 3: seller findAll ===");
-			list = sellerDao.findAll();
-			for (Seller obj : list) {
-				System.out.println(obj);
-			}
-			
-			System.out.println("\n=== TEST 4: seller Insert ===");
-			Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
-			sellerDao.insert(newSeller);
-			System.out.println("Inserted! New Id = " + newSeller.getId());
-
-
+		System.out.println("\n=== TEST 3: seller findAll ===");
+		list = sellerDao.findAll();
+		for (Seller obj : list) {
+			System.out.println(obj);
 		}
+
+		System.out.println("\n=== TEST 4: seller Insert ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New Id = " + newSeller.getId());
+
+		System.out.println("\n=== TEST 5: seller Update ===");
+
+		seller = sellerDao.findById(1);
+		seller.setName("Andressa Gomes");
+		seller.setEmail("andressitaPorroquita@gmail.com");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 
 	}
 
+}
